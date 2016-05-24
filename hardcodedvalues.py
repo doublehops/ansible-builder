6f8fe49ef1c33ef8798150a0197a60e4c47f7c1b
Damien Buttler <damien@doublehops.com>
2016-05-24 14:52:36 +1000

    Have Nginx in Centos working.

--- hardcodedvalues.py
+++ hardcodedvalues.py
@@ -4,7 +4,7 @@
 oses = { 'debian': { 'installer': 'apt', 'vagrantbox': 'debian/jessie64', 'vagrantbox_url': 'https://atlas.hashicorp.com/debian/boxes/jessie64' },
         'ubuntu': { 'installer': 'apt', 'vagrantbox': 'ubuntu/trusty64', 'vagrantbox_url': 'https://atlas.hashicorp.com/ubuntu/boxes/trusty64'},
          'redhat': { 'installer': 'yum', 'vagrantbox': '', 'vagrantbox_url': ''},
-         'centos6': { 'installer': 'yum', 'vagrantbox': 'centos/6', 'vagrantbox_url': 'https://github.com/2creatives/vagrant-centos/releases/download/v6.5.3/centos65-x86_64-20140116.box'},
+         'centos6': { 'installer': 'yum', 'vagrantbox': 'bento/centos-6.7', 'vagrantbox_url': 'https://atlas.hashicorp.com/bento/boxes/centos-6.7'},
          'centos7': { 'installer': 'yum', 'vagrantbox': 'centos/7', 'vagrantbox_url': 'https://atlas.hashicorp.com/centos/boxes/7'},
          'freebsd': { 'installer': 'yum', 'vagrantbox': 'freebsd/FreeBSD-10.2-STABLE', 'vagrantbox_url': ''},
 }
