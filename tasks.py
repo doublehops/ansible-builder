abbee8344ee33c7fe70056f665a973c776dbba94
Damien Buttler <damien@doublehops.com>
2016-05-25 11:30:14 +1000

    Not ordering tasks anymore.

--- tasks.py
+++ tasks.py
@@ -12,10 +12,10 @@ def createTasks(tasks, outputPath):
     # Process tasks in a specific order
     eval('include_add_ppas')(outputPath, tasks['add_ppas'])
     tasks.pop('add_ppas')
-    eval('include_webserver')(outputPath, tasks['webserver'])
-    tasks.pop('webserver')
-    eval('include_php7')(outputPath, tasks['php7'])
-    tasks.pop('php7')
+#    eval('include_webserver')(outputPath, tasks['webserver'])
+#    tasks.pop('webserver')
+#    eval('include_php7')(outputPath, tasks['php7'])
+#    tasks.pop('php7')
 
     # Process any remaining tasks where order not important
     for (key, value) in tasks.items():
