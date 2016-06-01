9463927942406d89e384ee058c5350431f2af805
Damien Buttler <damien@doublehops.com>
2016-06-01 14:57:36 +1000

    Cleaned up built entry script.

--- setupProvisioner.py
+++ setupProvisioner.py
@@ -6,18 +6,25 @@ def setupProvisioner(outputPath, vagrantHostname):
 
     print('-- Setting up provisioner --')
 
-    if os.path.isdir(outputPath +'/Vagrantfile'):
-        shutil.rmtree(outputPath +'/VagrantFile')
-    if os.path.isdir(outputPath +'/provisioners'):
-        shutil.rmtree(outputPath +'/provisioners')
+    removeExistingPaths(outputPath)
 
     copyTemplate('provisioners_core/playbook.yml', outputPath +'/provisioners/playbook.yml')
     devHostFile = outputPath +'/provisioners/ansible_hosts'
     copyTemplate('provisioners_core/ansible_hosts', devHostFile, {'vagrantHostname': vagrantHostname})
 
+    shutil.copytree('dev', outputPath +'/dev')
     shutil.copytree('provisioners_core/roles/common', outputPath +'/provisioners/roles/common')
 
 def cleanupProvisioner(outputPath):
 
     playbookFile = outputPath +'/provisioners/playbook.yml'
     copyTemplate(playbookFile, playbookFile, {'roles': ''})
+
+def removeExistingPaths(outputPath):
+
+    if os.path.isdir(outputPath +'/Vagrantfile'):
+        shutil.rmtree(outputPath +'/VagrantFile')
+    if os.path.isdir(outputPath +'/provisioners'):
+        shutil.rmtree(outputPath +'/provisioners')
+    if os.path.isdir(outputPath +'/dev'):
+        shutil.rmtree(outputPath +'/dev')
